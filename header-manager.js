c10259ab2053a3f16de7a3dd78688626c90bad21
Ezra Schwartz <ezra@artandtech.com>
2025-06-08 06:35:45 -0400

    Create header-manager.js

---
+++ header-manager.js
@@ -0,0 +1,42 @@
+<!-- ==========================================================================
+     SHARED HEADER TEMPLATE - USED ACROSS ALL PAGES
+     ========================================================================== -->
+
+<!-- Main Header Navigation -->
+<header id="mainHeader">
+  <div class="header-left">
+    <div class="logo">
+      <img src="ReVive-IMPACT-Logo.png" alt="ReVive IMPACT Logo" class="logo-image">
+    </div>
+    <nav id="mainNavigation">
+      <!-- Navigation tabs will be populated by JavaScript -->
+    </nav>
+  </div>
+
+  <div class="header-right" id="headerRight">
+    <!-- Search container - shown conditionally -->
+    <div class="search-container" id="searchContainer" style="display: none;">
+      <input type="text" class="search-input" id="clientSearchInput" placeholder=" ">
+      <span class="search-label">Client Search</span>
+      <div class="suggestions-container" id="searchSuggestions"></div>
+    </div>
+    
+    <!-- User info -->
+    <div class="user-info">
+      <span id="userName">Andrea Leflore</span>
+      <div class="user-avatar"></div>
+    </div>
+    <button class="sign-out">Sign Out</button>
+  </div>
+</header>
+
+<!-- Secondary Action Bar -->
+<div class="secondary-action-bar" id="secondaryActionBar">
+  <div class="secondary-action-bar-left" id="secondaryLeft">
+    <!-- Left filters will be populated by JavaScript -->
+  </div>
+  
+  <div class="secondary-action-bar-right" id="secondaryRight">
+    <!-- Right actions will be populated by JavaScript -->
+  </div>
+</div>
